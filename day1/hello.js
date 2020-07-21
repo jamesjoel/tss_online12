06d03c3097400f920946be8d5c081219fb142fba
jamesjoel <james.joelj@gmail.com>
2020-07-21 13:37:54 +0530

    first commit

---
+++ day1/hello.js
@@ -0,0 +1,19 @@
+/*
+string, int, float, boolean, array, object, null, undefined..
+
+
+*/
+var a = "rohit"; // string
+var b = 100; // int
+var c = 200.54; //  float
+var d = true; // boolean
+
+var data = ["red", "green", "blue", 100, 200, false]; // array
+
+var obj = {}; // empty object
+
+var obj = { name : "rohit", age : 25, city : "indore"};
+
+obj.state = "MP";
+
+console.log(obj);
