2b8d524138bca0e5be2afc0e1ce26a3e2f2260ba
jamesjoel <james.joelj@gmail.com>
2020-07-25 15:06:07 +0530

    add new folder for amit

---
+++ amit1/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'amit1';
+}
+
+
+/*
+
+ng serve ----------- localhost:4200
+ng serve --port = 5000 ---------- localhost:5000
+
+
+
+*/
